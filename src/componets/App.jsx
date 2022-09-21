7b2e0330a022bca16686fa4632b972b6f648e714
Prakashkumar Patel <patelprakash0508@gmail.com>
2022-09-21 11:47:49 +0200

    initial commit

---
+++ src/componets/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from './Footer';
+import Heading from './Header';
+import Note from './Note';
+
+function App(){
+return <div>
+    <Heading />
+    <Note  />
+    <Footer />
+</div>
+}
+export default App;
---
+++ src/componets/Note.jsx
@@ -0,0 +1,9 @@
+    import React from "react";
+    
+    function Note(){
+        return <div className="note">
+            <h1>Hello World</h1>
+            <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+        </div>
+    }
+    export default Note;
